718c3f6d7f9714f1436309617544c958657ec59f
tnt120 <apaj04@gmail.com>
2023-06-07 01:12:58 +0200

    Finished task

--- src/main/java/efs/task/collections/data/DataProvider.java
+++ src/main/java/efs/task/collections/data/DataProvider.java
@@ -3,6 +3,8 @@ package efs.task.collections.data;
 import efs.task.collections.entity.Hero;
 import efs.task.collections.entity.Town;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -15,12 +17,26 @@ public class DataProvider {
     //  Korzystając z funkcji split() oraz stałej DATA_SEPARATOR utwórz listę obiektów klasy efs.task.collections.entities.Town.
     //  Funkcja zwraca listę obiektów typu Town ze wszystkimi dziewięcioma podstawowymi miastami.
     public List<Town> getTownsList() {
-        return null;
+        List<Town> towns = new ArrayList<>(Data.baseTownsArray.length);
+
+        for (String town : Data.baseTownsArray) {
+            String[] townSplited = town.split(DATA_SEPARATOR);
+            towns.add(new Town(townSplited[0].trim(), List.of(townSplited[1].trim(), townSplited[2].trim())));
+        }
+
+        return towns;
     }
 
     //TODO Analogicznie do getTownsList utwórz listę miast na podstawie tablicy Data.DLCTownsArray
     public List<Town> getDLCTownsList() {
-        return null;
+        List<Town> towns = new ArrayList<>(Data.dlcTownsArray.length);
+
+        for (String town : Data.dlcTownsArray) {
+            String[] townSplited = town.split(DATA_SEPARATOR);
+            towns.add(new Town(townSplited[0].trim(), List.of(townSplited[1].trim(), townSplited[2].trim())));
+        }
+
+        return towns;
     }
 
     //TODO Na podstawie tablicy Data.baseCharactersArray utworzyć listę bohaterów dostępnych w grze.
@@ -28,11 +44,27 @@ public class DataProvider {
     // Korzystając z funkcji split() oraz DATA_SEPARATOR utwórz listę unikalnych obiektów efs.task.collections.entities.Hero.
     // UWAGA w Data.baseCharactersArray niektórzy bohaterowie powtarzają się, do porównania bohaterów używamy zarówno imie jak i jego klasę;
     public Set<Hero> getHeroesSet() {
-        return null;
+        Set<Hero> heroesSet = new HashSet<>();
+
+        for (String characterData : Data.baseCharactersArray) {
+            String[] characterDataSplit = characterData.split(DATA_SEPARATOR);
+            Hero newHero = new Hero(characterDataSplit[0].trim(), characterDataSplit[1].trim());
+            heroesSet.add(newHero);
+        }
+
+        return heroesSet;
     }
 
     //TODO Analogicznie do getHeroesSet utwórz listę bohaterów na podstawie tablicy Data.DLCCharactersArray
     public Set<Hero> getDLCHeroesSet() {
-        return null;
+        Set<Hero> heroesSet = new HashSet<>();
+
+        for (String characterData : Data.dlcCharactersArray) {
+            String[] characterDataSplit = characterData.split(DATA_SEPARATOR);
+            Hero newHero = new Hero(characterDataSplit[0].trim(), characterDataSplit[1].trim());
+            heroesSet.add(newHero);
+        }
+
+        return heroesSet;
     }
 }
--- src/main/java/efs/task/collections/entity/Hero.java
+++ src/main/java/efs/task/collections/entity/Hero.java
@@ -1,5 +1,7 @@
 package efs.task.collections.entity;
 
+import java.util.Objects;
+
 public class Hero {
     private String name;
     private String heroClass;
@@ -20,13 +22,22 @@ public class Hero {
     //TODO implementacja metody equal porównująca obiekty Hero na podstawie pól name i heroClass.
     @Override
     public boolean equals(Object o) {
-        return true;
+        if (this == o) {
+            return true;
+        }
+
+        if (getClass() != o.getClass() || o == null) {
+            return false;
+        }
+
+        Hero hero = (Hero) o;
+        return name.equals(hero.name) && heroClass.equals(hero.heroClass);
     }
 
     //TODO implementacja metody equal biorąca pod uwagę name i heroClass.
     @Override
     public int hashCode() {
-        return 1;
+        return Objects.hash(name, heroClass);
     }
 
     @Override
